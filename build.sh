f04a749c2b4de7d4c69bbb5c2d1c84e32d954c0a
killf <killf@foxmail.com>
2022-04-08 15:26:18 +0800

    添加llvm的cmake配置

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-export CLASSPATH="./lib/antlr-4.9.3-complete.jar:$CLASSPATH"
+#!/bin/sh
+export WORKSPACE="$(pwd)"
+export CLASSPATH="$WORKSPACE/lib/antlr-4.9.3-complete.jar:$CLASSPATH"
 
-java org.antlr.v4.Tool DaoLexer.g4 -o generated 
-java org.antlr.v4.Tool DaoParser.g4 -o generated -no-listener -visitor
+cd grammar
+java org.antlr.v4.Tool DaoLexer.g4 -o $WORKSPACE/generated 
+java org.antlr.v4.Tool DaoParser.g4 -o $WORKSPACE/generated -no-listener -visitor
